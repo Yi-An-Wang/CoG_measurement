4e7f994b6dfca38abbe54ec8dbd80bab372da7e2
Yi-An Wang <Wangia@solab.me.ntu.edu.tw>
2025-05-10 22:28:49 +0800

    <MMA8451><modified>_add a pitch roll angle reading part in read_MMA8451

--- python_code/read_data.py
+++ python_code/read_data.py
@@ -2,6 +2,7 @@ import serial
 import time
 from collections import deque
 import matplotlib.pyplot as plt
+from math import pi
 
 def main():
     ser = serial.Serial('COM3', 115200)
@@ -46,15 +47,21 @@ def read_hx711():
         plt.pause(0.01)
 
 def read_MMA8451():
-    ser = serial.Serial('COM3', 115200)
+    ser = serial.Serial('COM6', 115200)
     N = 100
     t_data = deque([0]*N, maxlen=N)
+
     ax_data = deque([0]*N, maxlen=N)
     ay_data = deque([0]*N, maxlen=N)
     az_data = deque([0]*N, maxlen=N)
     est_ax_data = deque([0]*N, maxlen=N)
     est_ay_data = deque([0]*N, maxlen=N)
     est_az_data = deque([0]*N, maxlen=N)
+
+    raw_pitch_data = deque([0]*N, maxlen=N)
+    raw_roll_data = deque([0]*N, maxlen=N)
+    pitch_data = deque([0]*N, maxlen=N)
+    roll_data = deque([0]*N, maxlen=N)
 
     fig = plt.figure()
     axis_x = fig.add_subplot(311)
@@ -73,6 +80,18 @@ def read_MMA8451():
     axis_y.legend()
     axis_z.legend()
 
+    fig2 = plt.figure()
+    axis_pitch = fig2.add_subplot(211)
+    axis_roll = fig2.add_subplot(212)
+    axis_pitch.grid(True)
+    axis_roll.grid(True)
+    line_raw_pitch, = axis_pitch.plot(t_data, raw_pitch_data, label="raw_pitch")
+    line_raw_roll, = axis_roll.plot(t_data, raw_roll_data, label="raw_roll")
+    line_pitch, = axis_pitch.plot(t_data, pitch_data, label="pitch")
+    line_roll, = axis_roll.plot(t_data, roll_data, label="roll")
+    axis_pitch.legend()
+    axis_roll.legend()
+
     t_pre = time.time()
     t = 0.0
     plt.ion()
@@ -82,16 +101,25 @@ def read_MMA8451():
         values = line.split(",")
         ax, ay, az = map(float, values[0:3])
         est_ax, est_ay, est_az = map(float, values[3:6])
+        raw_pitch, raw_roll = map(float, values[6:8])
+        pitch, roll = map(float, values[8:10])
+
         t_now = time.time()
         t = t + t_now - t_pre
         t_data.append(t)
         t_pre = t_now
+
         ax_data.append(ax)
         ay_data.append(ay)
         az_data.append(az)
         est_ax_data.append(est_ax)
         est_ay_data.append(est_ay)
         est_az_data.append(est_az)
+
+        raw_pitch_data.append(raw_pitch * 180 / pi)
+        raw_roll_data.append(raw_roll * 180 / pi)
+        pitch_data.append(pitch * 180 / pi)
+        roll_data.append(roll * 180 / pi)
 
         line_ax.set_data(t_data, ax_data)
         line_ay.set_data(t_data, ay_data)
@@ -100,12 +128,22 @@ def read_MMA8451():
         line_est_ay.set_data(t_data, est_ay_data)
         line_est_az.set_data(t_data, est_az_data)
 
+        line_raw_pitch.set_data(t_data, raw_pitch_data)
+        line_raw_roll.set_data(t_data, raw_roll_data)
+        line_pitch.set_data(t_data, pitch_data)
+        line_roll.set_data(t_data, roll_data)
+
         axis_x.relim()
         axis_x.autoscale_view()
         axis_y.relim()
         axis_y.autoscale_view()
         axis_z.relim()
         axis_z.autoscale_view()
+
+        axis_pitch.relim()
+        axis_pitch.autoscale_view()
+        axis_roll.relim()
+        axis_roll.autoscale_view()
 
         plt.pause(0.01)
 
